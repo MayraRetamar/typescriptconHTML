016816f434f0c236de40d3596e5e220018414a3c
MayraRetamar <104107235+MayraRetamar@users.noreply.github.com>
2022-07-29 14:03:07 -0300

    agrgueCodigo

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
 let btnMenos = document.getElementById("menos");
 let btnMas = document.getElementById("mas");
+let div = document.getElementById("cantidad");
 
 let contador: number = 0;
 
 function contarClicks() {
   contador++;
+  div?.innerHTML = contador;
+  console.log(contador + " clicks");
+}
+function sacarClicks() {
+  contador = contador - 1;
+  div?.innerHTML = contador;
   console.log(contador + " clicks");
 }
 
 btnMas?.addEventListener("click", contarClicks);
-
-function sacarClicks() {
-  contador = contador - 1;
-  console.log(contador + " clicks");
-}
 btnMenos?.addEventListener("click", sacarClicks);
